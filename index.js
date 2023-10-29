e620b483def20d17cc8063fa27b216838ea1a111
unknown <almeidasantoswelisson2@gmail.com>
2023-10-29 09:02:33 -0300

    versão inicial

---
+++ index.js
@@ -0,0 +1,19 @@
+var setadireita = window.document.getElementById(seta-direita)
+var setaesquerda =window.document.getElementById(seta-esquerda)
+var Leonardo = window.document.getElementById(Leonardo)
+var samantha =window.document.getElementById(samantha)
+var bruna =window.document.getElementById(bruna)
+
+function RolarParaDireita() {
+    leonardo.style = "display:none"
+    bruna.style = "display:flex"
+    setadireita.style = "display:none"
+    setaesquerda.style = "display:flex ; margin-top:55px"    
+}
+ function RolarParaEsquerda() {
+Leonardo.style = "display:flex"
+bruna.style = "display:none"
+setadireita.style = "display:flex ; margin-top:55px"
+setaesquerda.style = "display:none"
+
+ }
